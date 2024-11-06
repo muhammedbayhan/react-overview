7c7b4ed6114b347daaf9dd291fcefe79abda05bc
Muhammed Bayhan <muhammedbayhan72@gmail.com>
2024-11-07 00:53:55 +0300

    state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 const PostList = () => {
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleBodyChange = (e) => {
+    setBody(e.target.value);
+  };
+
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Muhammed" body="Bayhannn" />
-      <Post author="Mehmet" body="Arif Bayhan" />
-    </ul>
+    <>
+      <NewPost
+        handleBodyChange={handleBodyChange}
+        handleAuthorChange={handleAuthorChange}
+      />
+      <ul className={classes.posts}>
+        <Post author={author} body={body} />
+        <Post author="Mehmet" body="Arif Bayhan" />
+      </ul>
+    </>
   );
 };
+
 export default PostList;
